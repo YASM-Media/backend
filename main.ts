6824899f7d8a8e43caf6ae2bb819cfd3db10a458
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2025-05-01 20:06:57 +0530

    feat(valkey): valkey ponging my ping

--- main.ts
+++ main.ts
@@ -1,8 +1,11 @@
 import { Context, Hono } from "hono";
+import { fetchCacheClient } from "./cache/client.ts";
 
 const app = new Hono();
 
-app.get("/", (c: Context) => {
+app.get("/", async (c: Context) => {
+  const cacheClient = await fetchCacheClient();
+  console.log(await cacheClient.sendCommand(["PING"]));
   return c.text("Hello Hono!");
 });
 

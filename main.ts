be048de69b73d330990f399d312e18ec04ba32f3
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2025-05-03 14:50:03 +0530

    feat(backend): setup authentication using keycloak jwks uri

--- main.ts
+++ main.ts
@@ -1,7 +1,15 @@
 import { Context, Hono } from "hono";
+import { jwk } from "hono/jwk";
 import { fetchCacheClient } from "./cache/client.ts";
 
 const app = new Hono();
+
+app.use(
+  "*",
+  jwk({
+    jwks_uri: Deno.env.get("AUTHENTICATION_JWKS")!,
+  }),
+);
 
 app.get("/", async (c: Context) => {
   const cacheClient = await fetchCacheClient();

63c495a1b029e6a44df9ef95d54ed23911d8e762
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2025-05-18 15:02:08 +0530

    [TASK] Database modules setup for Authentication and User Info (#17)
    # Implemented Changes
    - Database modules setup for Authentication and User Info
    - Authentication Info completed with the following:
        - Keycloak ID
    - User Info completed with the following:
        - First name
        - Last name
    - Authentication and User Info has one on one relationship with each
    other
    
    **Closes: #11**

---
+++ database/tables/schema.ts
@@ -0,0 +1,54 @@
+import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+
+// Created and Updated Column Names
+const createdAt = timestamp("created_at", { withTimezone: true })
+  .notNull()
+  .defaultNow();
+const updatedAt = timestamp("updated_at", { withTimezone: true })
+  .notNull()
+  .defaultNow()
+  .$onUpdate(() => new Date());
+
+// ------------ AUTHENTICATION INFO TABLE ------------ //
+export const authenticationInfo = pgTable("authentication_info", {
+  keycloakId: uuid("keycloak_id").primaryKey().notNull().unique(),
+  createdAt,
+  updatedAt,
+});
+
+export type AuthenticationInfo = typeof authenticationInfo.$inferSelect;
+
+// ------------ USER INFO TABLE ------------ //
+export const userInfo = pgTable("user_info", {
+  id: uuid().primaryKey().notNull().defaultRandom(),
+  firstName: text("first_name").notNull(),
+  lastName: text("last_name").notNull(),
+  keycloakId: uuid("keycloak_id").references(() =>
+    authenticationInfo.keycloakId
+  ),
+  createdAt,
+  updatedAt,
+});
+
+export type UserInfo = typeof userInfo.$inferInsert;
+
+// ------------ RELATIONS ------------ //
+
+// Authentication Info <----ONE----> User Info
+export const authenticationInfoUserInfoRelation = relations(
+  authenticationInfo,
+  ({ one }) => ({
+    userInfo: one(userInfo),
+  }),
+);
+
+export const userInfoAuthenticationInfoRelation = relations(
+  userInfo,
+  ({ one }) => ({
+    authenticationInfo: one(authenticationInfo, {
+      fields: [userInfo.keycloakId],
+      references: [authenticationInfo.keycloakId],
+    }),
+  }),
+);

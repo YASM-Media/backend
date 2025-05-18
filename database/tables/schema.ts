798f34a31283738a402a5823a1bca250e2af9b8c
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2025-05-18 14:44:14 +0530

    feat(database): Authentication Info Schema Implementation

--- database/tables/schema.ts
+++ database/tables/schema.ts
@@ -10,3 +10,11 @@ const updatedAt = timestamp("updated_at", { withTimezone: true })
   .defaultNow()
   .$onUpdate(() => new Date());
 
+// ------------ AUTHENTICATION INFO TABLE ------------ //
+export const authenticationInfo = pgTable("authentication_info", {
+  keycloakId: uuid("keycloak_id").primaryKey().notNull().unique(),
+  createdAt,
+  updatedAt,
+});
+
+export type AuthenticationInfo = typeof authenticationInfo.$inferSelect;

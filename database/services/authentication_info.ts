e34cc1970a8a97c254a21c9d3277907dece3e5ad
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2025-05-24 14:17:05 +0530

    feat(authentication info service): select many authentication info service

--- database/services/authentication_info.ts
+++ database/services/authentication_info.ts
@@ -1,6 +1,8 @@
 import { database } from "../database.ts";
 import { eq } from "drizzle-orm";
 import { authenticationInfo } from "../tables/schema.ts";
+import type { SQL } from "drizzle-orm";
+
 // Select One Authentication Info with Keycloak ID
 export const selectOneAuthenticationInfo = (keycloakId: string) =>
   database.query.authenticationInfo.findFirst({
@@ -9,3 +11,18 @@ export const selectOneAuthenticationInfo = (keycloakId: string) =>
       userInfo: true,
     },
   });
+
+// Select Many Authentication Info based on a custom condition
+export const selectManyAuthenticationInfo = (condition: SQL | void) =>
+  condition != null
+    ? database.query.authenticationInfo.findMany({
+      where: condition,
+      with: {
+        userInfo: true,
+      },
+    })
+    : database.query.authenticationInfo.findMany({
+      with: {
+        userInfo: true,
+      },
+    });

b3935c1909869539637e5e3ba60e9b18dfde5c64
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2025-07-13 16:55:41 +0530

    feat(main): implementation of router to point towards correct controller

--- main.ts
+++ main.ts
@@ -2,9 +2,12 @@ import { Context, Hono } from "hono";
 import { jwk } from "hono/jwk";
 import { cors } from "hono/cors";
 import { userinfo } from "./middleware/userinfo.ts";
+import auth from "./controllers/auth/auth.ts";
 
+// Main Application Router
 const app = new Hono();
 
+// CORS Middleware
 app.use(
   cors({
     origin: Deno.env.get("CORS_ORIGIN_WHITELIST")!.split(","),
@@ -15,16 +18,21 @@ app.use(
   }),
 );
 
+// JWKS Middleware for Authentication
 app.use(
   jwk({
     jwks_uri: Deno.env.get("AUTHENTICATION_JWKS")!,
   }),
 );
 
+// Userinfo Middleware for fetching User Info from Keycloak
 app.use(userinfo);
 
 app.get("/", (c: Context) => {
-  return c.json(JSON.parse(c.get("userinfo")));
+  return c.json(JSON.parse(c.get("authenticationinfo")));
 });
+
+// ------------- Controllers for Different Modules ------------- #
+app.route("/", auth);
 
 Deno.serve({ port: 9080 }, app.fetch);
